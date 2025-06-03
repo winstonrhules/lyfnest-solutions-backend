ae1b984f65b5446a16c5547992373fcc8d072b8e
winston rhules <winstonrhules@gmail.com>
2025-06-03 05:13:39 +0000

    skemishes

--- middlewares/corsConfig.js
+++ middlewares/corsConfig.js
@@ -1,6 +1,6 @@
 const cors = require('cors')
 const corsOption = {
-    origin:['https://life.lyfnestsolutions.com', 'http://localhost:5174',  'http://localhost:5173'],
+    origin:['https://life.lyfnestsolutions.com', 'https://natbetadmin.lyfnestsolutions.com', 'http://localhost:5174',  'http://localhost:5173'],
     credentials:true
 }
 
