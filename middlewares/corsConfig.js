6f94e818e3835a9c9ae7452e43ab798226013611
winston rhules <winstonrhules@gmail.com>
2025-06-04 03:27:10 +0000

    cors changes

--- middlewares/corsConfig.js
+++ middlewares/corsConfig.js
@@ -1,6 +1,6 @@
 const cors = require('cors')
 const corsOption = {
-    origin:[process.env.CORS_ORIGIN, process.env.LFRONT_URL_ORIGIN, process.env.CORS_ORIGIN_ADMIN, process.env.LADMIN_URL_ORIGIN ],
+    origin:[process.env.CORS_ORIGIN, 'http://localhost:5174', process.env.CORS_ORIGIN_ADMIN,   'http://localhost:5173'],
     credentials:true
 }
 
