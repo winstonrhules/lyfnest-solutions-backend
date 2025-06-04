5dbb6ef06156a88c412f7692496a89b3cc804147
winston rhules <winstonrhules@gmail.com>
2025-06-04 04:10:56 +0000

    cors again

--- middlewares/corsConfig.js
+++ middlewares/corsConfig.js
@@ -1,6 +1,6 @@
 const cors = require('cors')
 const corsOption = {
-    origin:[process.env.CORS_ORIGIN, 'http://localhost:5174', process.env.CORS_ORIGIN_ADMIN,   'http://localhost:5173'],
+    origin:['https://life.lyfnestsolutions.com',  'http://localhost:5174', 'https://natbetadmin.lyfnestsolutions.com',  'http://localhost:5173'],
     credentials:true
 }
 
