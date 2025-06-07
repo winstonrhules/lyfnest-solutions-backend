0bb1a3c8f0e46d6e08297260982251e74630a3b8
winston rhules <winstonrhules@gmail.com>
2025-06-07 00:39:44 +0000

    steeze with cors

--- middlewares/corsConfig.js
+++ middlewares/corsConfig.js
@@ -1,6 +1,6 @@
 const cors = require('cors')
 const corsOption = {
-    origin:['https://life.lyfnestsolutions.com',  'http://localhost:5174', 'https://natbetadmin.lyfnestsolutions.com',  'http://localhost:5173'],
+    origin:['https://life.lyfnestsolutions.com',  'http://localhost:5174', 'https://natbetadmin.lyfnestsolutions.com',  process.env.CORS_ORIGIN_ADMIN],
     credentials:true
 }
 
