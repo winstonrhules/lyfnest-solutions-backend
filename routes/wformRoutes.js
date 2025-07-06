55c15637dfa1d892deb91d4912e46628beea8037
winston rhules <winstonrhules@gmail.com>
2025-07-06 01:11:52 +0000

    hello error

--- routes/wformRoutes.js
+++ routes/wformRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 //const verificationLimiter = require('../middlewares/rateLimiter');
-const {initialVerificationChecks, sendEmailVerification, verifyCode, verifyEmailCode, submissionForm, getallWforms,  getAllNotifs, createNotifs, deleteNotifs, deleteANotifs} = require('../controllers/wformController');
+const {initialVerificationChecks, sendEmailVerification, verifyCode, verifyEmailCode, submissionForm, getallWforms,  getAllNotifs, createNotifs, contactUserByEmail,  deleteNotifs, deleteANotifs} = require('../controllers/wformController');
 
 
 router.post('/start-verification',   initialVerificationChecks);
@@ -11,6 +11,7 @@ router.get('/all-forms', getallWforms)
 router.post('/send-email-verification', sendEmailVerification);
 router.post('/verify-email-code', verifyEmailCode);
 router.post('/create-notifs',  createNotifs);
+router.post('/contact-email', contactUserByEmail);
 // router.get('/get-notifs', getNotifs)
 router.get('/get-all-notifs', getAllNotifs,)
 router.delete('/delete-notifs',  deleteNotifs);
