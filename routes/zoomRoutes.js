5877cfe8d26af9a023aecf2a0c5319c18f43069b
winston rhules <winstonrhules@gmail.com>
2025-07-29 18:47:05 +0000

    zoom error fixed

--- routes/zoomRoutes.js
+++ routes/zoomRoutes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { syncZoomMeetings } = require('../controllers/zoomController');
 
+
+router.post('/create-meeting', async (req, res) => {
+  try {
+    await syncZoomMeetings();
+    res.send('Zoom meetings created.');
+  } catch (e) {
+    res.status(500).send('Zoom meeting creation failed');
+  }
+})
+
 router.get('/sync', async (req, res) => {
   try {
     await syncZoomMeetings();
@@ -10,5 +20,6 @@ router.get('/sync', async (req, res) => {
     res.status(500).send('Zoom sync failed');
   }
 });
+
 
 module.exports = router;
