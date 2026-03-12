65b695035faf65a7254389fec4200f664a7eaee8
winston rhules <winstonrhules@gmail.com>
2026-03-12 05:30:00 +0000

    Fixed issues

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -26,6 +26,34 @@
 // module.exports = router;
 
 
+// const express = require('express');
+// const router = express.Router();
+// const {
+//   getAllAppointments,
+//   updateAppointmentStatus,
+//   rescheduleAppointment,
+//   deleteAppointmentWithZoom,
+//   markAppointmentCompleted,
+//   handleZoomWebhook,
+//   scheduleContactListZoomMeeting,
+//   bookContactMeeting
+// } = require('../controllers/appointmentController');
+
+// // Existing routes
+// router.get('/all-appointment', getAllAppointments);
+// router.put('/update-status/:id', updateAppointmentStatus);
+// router.put('/reschedule/:id', rescheduleAppointment);
+// router.put('/:id/complete', markAppointmentCompleted);
+// router.delete('/delete-appointment/:id', deleteAppointmentWithZoom);
+// router.post('/zoom-webhook', handleZoomWebhook);
+
+// // NEW ROUTES for contact list functionality
+// router.post('/schedule-contact-zoom', scheduleContactListZoomMeeting);
+// router.post('/book-contact-meeting/:appointmentId', bookContactMeeting);
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
 const {
@@ -52,6 +80,3 @@ router.post('/schedule-contact-zoom', scheduleContactListZoomMeeting);
 router.post('/book-contact-meeting/:appointmentId', bookContactMeeting);
 
 module.exports = router;
-
-
-
